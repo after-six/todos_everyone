83aa9c88d1943429cad2c0396c438fa5cdfcc1b1
Hyeon-Mook Jerry Choi <chm073@gmail.com>
2020-04-29 19:25:38 +0900

    Done insert and remove

--- lib/main.dart
+++ lib/main.dart
@@ -8,107 +8,85 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    List<String> aaaa = ["blablalbla","blablalbla","blablalbla","blablalbla","blablalbla","blablalbla","blablalbla","blablalbla","blablalbla",]
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-          primarySwatch: Colors.red, secondaryHeaderColor: Colors.orange),
-      home: Scaffold(
-        appBar: AppBar(title: Text("After todo")),
-        body: Column(
-          children: <Widget>[
-            TodoTextField(),
-            Expanded(
-              child: ListView(
-                children: <Widget>[
-                  ListTile(
-                    title: Text("blablalbla"),
+    return MaterialApp(title: 'Flutter Demo', theme: ThemeData(primarySwatch: Colors.red, secondaryHeaderColor: Colors.orange), home: TodoListWidget());
+  }
+}
+
+class TodoState extends State<TodoListWidget> {
+  List<String> todoItems = ["abababab", "blablalbla", "ababrbrb", "blablalbla", "rstbrstbrstbrstbrstb", "blablbrststbalbla", "blablalbla"];
+
+  TextEditingController _textController = TextEditingController();
+
+  TextField _textField;
+
+  @override
+  Widget build(BuildContext context) {
+    _textField = TextField(
+      maxLength: 25,
+      controller: _textController,
+      onSubmitted: (text) {
+        _textController.clear();
+        setState(() {
+          todoItems.insert(0, text);
+        });
+      },
+      decoration: InputDecoration(counterStyle: TextStyle(fontSize: 0), hintText: "What do you gonna do?", border: InputBorder.none),
+    );
+
+
+    return Scaffold(
+      appBar: AppBar(title: Text("After todo")),
+      body: Column(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.amber,
+              border: Border.all(color: Colors.grey, width: 2),
+            ),
+            child: Row(
+              children: <Widget>[
+                Flexible(
+                  flex: 9,
+                  child: _textField
+                ),
+                Flexible(
+                  flex: 1,
+                  child: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        todoItems.insert(0, _textController.text);
+                      });
+                      _textController.clear();
+                    },
+                    icon: Icon(
+                      Icons.send,
+                      color: Colors.red,
+                    ),
                   ),
-                  ListTile(
-                    title: Text("blablalbla"),
-                  ),
-                  ListTile(
-                    title: Text("blablalbla"),
-                  ),
-                  ListTile(
-                    title: Text("blablalbla"),
-                  ),
-                  ListTile(
-                    title: Text("blablalbla"),
-                  ),
-                  ListTile(
-                    title: Text("blablalbla"),
-                  ),
-                  ListTile(
-                    title: Text("blablalbla"),
-                  ),
-                  ListTile(
-                    title: Text("blablalbla"),
-                  ),
-                  ListTile(
-                    title: Text("blablalbla"),
-                  ),
-                  ListTile(
-                    title: Text("blablalbla"),
-                  ),
-                  ListTile(
-                    title: Text("blablalbla"),
-                  ),
-                  ListTile(
-                    title: Text("blablalbla"),
-                  ),
-                  ListTile(
-                    title: Text("blablalbla"),
-                  ),
-                  ListTile(
-                    title: Text("This is end"),
-                  ),
-                ],
-              ),
-            )
-          ],
-        ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: todoItems.length,
+              itemBuilder: (context, index) {
+                return ListTile(title: Text(todoItems[index]), onTap: () {
+                  print(index);
+                  setState(() {
+                    todoItems.removeAt(index);
+                  });
+                });
+              },
+            ),
+          )
+        ],
       ),
     );
   }
 }
 
-class TodoTextField extends StatelessWidget {
-  const TodoTextField({
-    Key key,
-  }) : super(key: key);
-
+class TodoListWidget extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.amber,
-        border: Border.all(color: Colors.grey, width: 2),
-      ),
-      child: Row(
-        children: <Widget>[
-          Flexible(
-            flex: 9,
-            child: TextField(
-              maxLength: 25,
-              decoration: InputDecoration(
-                counterStyle: TextStyle(fontSize: 0),
-                hintText: "What do you gonna do?",
-                border: InputBorder.none
-              ),
-            ),
-          ),
-          Flexible(
-            flex: 1,
-            child: IconButton(
-              icon: Icon(
-                Icons.send,
-                color: Colors.red,
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
+  State<TodoListWidget> createState() => TodoState();
 }
